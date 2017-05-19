886ccf00f197ba212bda68e876d0702c23a7d743
Mikael Vesavuori <mikael.vesavuori@sogeti.com>
2017-05-19 13:37:59 +0200

    Update

--- js/site.js
+++ js/site.js
@@ -13,4 +13,32 @@ function eventHandler(e) {
 	if (e.classList.contains("menu-btn")) {
 		e.nextElementSibling.classList.toggle("isOpen");
 	}
+
+	if (e.tagName === "IMG" || e.classList.contains("imageviewer") || e.classList.contains("imageviewer--content")) {
+		handlePopup(e);
+	}
+}
+
+function handlePopup(e) {
+	if (e === null || e === undefined || e === "") {
+		toggleModal();
+	}
+
+	else {
+		if (e.src !== undefined) {
+			let url = e.src;
+			let image = document.querySelector(".imageviewer--image");
+			image.src = url;
+		}
+
+		toggleModal();
+	}
+}
+
+function toggleModal() {
+	let body = document.querySelector("body");
+	let viewer = document.querySelector(".imageviewer");
+
+	body.classList.toggle("noscroll");
+	viewer.classList.toggle("active");
 }
